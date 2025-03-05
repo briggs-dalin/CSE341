df2d5dc7f4e09a17c7de8d818bc8d5353ccbca58
Dalin Briggs <dtox568@byui.edu>
2025-03-05 12:50:21 -0700

    Added routes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // express web server
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
- 
-app.get('/', lesson1Controller.klaraRoute);
-app.get('/dalin', lesson1Controller.dalinRoute);
 
 const port = 3000;
+
+app.use('/', require('./routes/index'));
  
 app.listen(process.env.port || port);
   console.log('Web Server is listening at port ' + (process.env.port || port));
